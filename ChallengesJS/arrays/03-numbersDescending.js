10914ae88499fe9e7ba8a17a577d7028d67ccb74
Miguel Cutri <vitor.cutri@hotmail.com>
2023-12-05 10:54:00 -0300

    adjustments in exercices

--- ChallengesJS/arrays/03-numbersDescending.js
+++ ChallengesJS/arrays/03-numbersDescending.js
@@ -1,11 +1,5 @@
 const numbers = [5, 7, 1, 8, 9];
 const numbersGrowing = numbers.sort((a, b) => {
-  if (a < b) {
-    return 1;
-  }
-  if (a > b) {
-    return -1;
-  }
-  return 0;
+  return b - a;
 });
 console.log(numbersGrowing);
--- ChallengesJS/arrays/04-oddAndEven.js
+++ ChallengesJS/arrays/04-oddAndEven.js
@@ -1,4 +1,8 @@
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numeros = [];
+const tamanho = 10;
+for (let i = 1; i <= tamanho; i++) {
+  numeros.push(i);
+}
 
 const pares = numeros.filter((pares) => {
   return pares % 2 === 0;
@@ -7,5 +11,9 @@ const impares = numeros.filter((impares) => {
   return impares % 2 !== 0;
 });
 
-console.log(`Números que são pares: ${pares}, Quantidade de números pares: ${pares.length}`);
-console.log(`Números que são ímpares: ${impares}, Quantidade de números ímpares: ${impares.length}`);
+console.log(
+  `Números que são pares: ${pares}, Quantidade de números pares: ${pares.length}`
+);
+console.log(
+  `Números que são ímpares: ${impares}, Quantidade de números ímpares: ${impares.length}`
+);

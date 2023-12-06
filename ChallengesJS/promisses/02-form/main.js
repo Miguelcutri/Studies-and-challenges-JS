86186da67c2f5a6c4234c7cfc2a3a9771007bccf
Miguel Cutri <vitor.cutri@hotmail.com>
2023-12-06 14:07:36 -0300

    exercice 2 complete

---
+++ ChallengesJS/promisses/02-form/main.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const button = document.getElementById("btn");
+const title = document.getElementById("title");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  if (input.value === "") {
+    alert("Preencha o CEP");
+    return;
+  }
+
+  await pegarDados();
+});
+
+async function pegarDados() {
+  try {
+    const response = await fetch(
+      `https://viacep.com.br/ws/${input.value}/json/`
+    );
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(`Erro de HTTP, status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    title.innerHTML = `${json.logradouro} | ${json.bairro} | ${json.localidade} | ${json.uf}`;
+  } catch (error) {
+    console.error("Erro ao buscar dados: ", error);
+    title.innerHTML = "Erro ao carregar dados";
+  }
+}
